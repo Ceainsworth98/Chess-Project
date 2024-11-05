61f1ebf3b9165adf0e4963d3caf1efcc84039ff3
Carissa Ainsworth <carissa.ainsworth@gmail.com>
2024-11-04 18:52:15 -0800

    Add files via upload

---
+++ Source-2.cpp
@@ -0,0 +1,20 @@
+//Carissa Ainsworth
+//Chess Project Step 4
+//12/9/2021
+
+#include <iostream>
+#include <string>
+#include "Moveclass.h"
+
+using namespace std;
+int main()
+{
+    string arr[8][8];
+    int chooserow, choosecol; //user imput 
+    int r=0, c=0;
+
+    Moveclass move1;
+    move1.board_display(arr, r, c);
+    move1.peice_move();
+    return 0;
+}
